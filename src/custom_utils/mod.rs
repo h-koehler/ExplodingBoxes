71c0939c73f90e0c5738756942b29293a8abfe75
Dominique Smith-Rodriguez <smithrods59@gmail.com>
2025-11-16 11:25:10 -0500

    Pause menu :D

--- src/custom_utils/mod.rs
+++ src/custom_utils/mod.rs
@@ -8,21 +8,44 @@ pub enum GameState {
     BossCatTime,
 }
 
+#[derive(Component)]
+pub struct PauseMenu;
+
 fn toggle_pause(
     mut next_state: ResMut<NextState<GameState>>,
+    mut commands: Commands,
+    q_pause: Query<Entity, With<PauseMenu>>,
+    asset_server: Res<AssetServer>,
     keyboard: Res<ButtonInput<KeyCode>>,
     state: Res<State<GameState>>,
 ) {
     if keyboard.just_pressed(KeyCode::Escape) {
         match state.get() {
-            GameState::Running => next_state.set(GameState::Paused),
-            GameState::Paused => next_state.set(GameState::Running),
+            GameState::Running => {
+                next_state.set(GameState::Paused);
+                commands.spawn((
+                    PauseMenu,
+                    Node {
+                        margin: UiRect::all(Val::Auto),
+                        width: Val::Px(600.0),
+                        height: Val::Px(600.0),
+                        ..Default::default()
+                    },
+                    ImageNode::new(asset_server.load("ui_elements/pause_menu.png")),
+                ));
+            }
+            GameState::Paused => {
+                next_state.set(GameState::Running);
+                if let Ok(pause_ent) = q_pause.single() {
+                    commands.entity(pause_ent).despawn();
+                }
+            }
             GameState::BossCatTime => next_state.set(GameState::Paused),
         }
     }
 }
 
 pub(super) fn register(app: &mut App) {
-        app.add_systems(Update, toggle_pause);
-        app.init_state::<GameState>();
+    app.add_systems(Update, toggle_pause);
+    app.init_state::<GameState>();
 }
