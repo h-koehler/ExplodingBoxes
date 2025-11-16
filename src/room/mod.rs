537a738ad4699e18587d16c2c1efc1aeca7d3b2d
AnthonyTornetta <cornchipgonecodin@gmail.com>
2025-11-15 21:03:31 -0500

    Fix merge conflict

--- src/room/mod.rs
+++ src/room/mod.rs
@@ -280,5 +280,8 @@ fn load_sprites(mut commands: Commands, asset_loader: Res<AssetServer>) {
 
 pub(super) fn register(app: &mut App) {
     app.add_systems(Startup, (setup_room, load_sprites));
-    app.add_systems(Update, (animate_conveyors, move_thing_on_conveyor).run_if(in_state(GameState::Running));
+    app.add_systems(
+        Update,
+        (animate_conveyors, move_thing_on_conveyor).run_if(in_state(GameState::Running)),
+    );
 }
