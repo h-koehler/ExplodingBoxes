15005489706ac947a12bcb8c07afe9534fc30264
AnthonyTornetta <cornchipgonecodin@gmail.com>
2025-11-15 21:21:47 -0500

    Level advancing

--- src/levels/mod.rs
+++ src/levels/mod.rs
@@ -2,6 +2,8 @@ use bevy::{color::palettes::css, prelude::*};
 
 use crate::boxes::spawn::{SpawnItem, SpawnList};
 
+pub mod advance;
+
 #[derive(Resource, Clone, Copy)]
 pub enum Level {
     One,
@@ -9,6 +11,20 @@ pub enum Level {
     Three,
     Four,
     Five,
+}
+
+impl Level {
+    pub fn advance(&mut self) -> bool {
+        match self {
+            Self::One => Self::Two,
+            Self::Two => Self::Three,
+            Self::Three => Self::Four,
+            Self::Four => Self::Five,
+            Self::Five => return true,
+        };
+
+        false
+    }
 }
 
 fn setup_level(mut commands: Commands, level: Res<Level>) {
@@ -48,6 +64,8 @@ fn setup_level(mut commands: Commands, level: Res<Level>) {
 }
 
 pub(super) fn register(app: &mut App) {
+    advance::register(app);
+
     app.add_systems(Update, setup_level.run_if(resource_changed::<Level>))
         .insert_resource(Level::One);
 }
